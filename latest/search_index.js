780a52144d2f98c12ffa7e97c5440b6301f5767b
autodocs <autodocs>
2017-06-30 19:46:59 +0000

    build based on cb0e5ce

--- latest/search_index.js
+++ latest/search_index.js
@@ -73,11 +73,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "pages/manual.html#Setup-1",
+    "page": "Manual",
+    "title": "Setup",
+    "category": "section",
+    "text": "To use this package you need to also install Dask.Distributed."
+},
+
+{
     "location": "pages/manual.html#Usage-1",
     "page": "Manual",
     "title": "Usage",
     "category": "section",
-    "text": "To use this package you need to install Dask.Distributed and start a dask-scheduler process:$ dask-scheduler\nScheduler started at 127.0.0.1:8786Start a julia process and startup a cluster of julia client/workers providing them the scheduler's address:using DaskDistributedDispatcher\naddprocs()\n@everywhere using DaskDistributedDispatcher\nclient = Client(\"127.0.0.1:8786\")\n@spawn worker = Worker(\"127.0.0.1:8786\")\n@spawn worker = Worker(\"127.0.0.1:8786\")Then, you can submit Dispatcher Ops units of computation that can be run to the client which will relay it to the dask-scheduler to be scheduled and executed on a worker.op = Dispatcher.Op(Int, 2.0)\nsubmit(client, op)\nresult = result(client, op)"
+    "text": "First start a dask-scheduler process:$ dask-scheduler\nScheduler started at 127.0.0.1:8786Then, start a julia process and set up a cluster of julia client/workers providing them the scheduler's address:using DaskDistributedDispatcher\naddprocs()\n@everywhere using DaskDistributedDispatcher\nclient = Client(\"127.0.0.1:8786\")\n@spawn worker = Worker(\"127.0.0.1:8786\")\n@spawn worker = Worker(\"127.0.0.1:8786\")Then, you can submit Dispatcher Ops units of computation that can be run to the client which will relay it to the dask-scheduler to be scheduled and executed on a worker.op = Dispatcher.Op(Int, 2.0)\nsubmit(client, op)\nresult = result(client, op)"
 },
 
 {
@@ -94,70 +102,6 @@ var documenterSearchIndex = {"docs": [
     "title": "API",
     "category": "section",
     "text": ""
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.Client",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.Client",
-    "category": "Type",
-    "text": "Client\n\nA Client represents a client that the user can interact with to submit computations to the scheduler and gather results.\n\n\n\n"
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.Client-Tuple{String}",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.Client",
-    "category": "Method",
-    "text": "Client(scheduler_address::String) -> Client\n\nConstruct a Client which can then be used to submit computations or gather results from the dask-scheduler process.\n\n\n\n"
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.submit-Tuple{DaskDistributedDispatcher.Client,Dispatcher.Op}",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.submit",
-    "category": "Method",
-    "text": "submit(client::Client, op::Dispatcher.Op)\n\nSubmit the Op computation unit to the dask-scheduler for computation.\n\n\n\n"
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.result-Tuple{DaskDistributedDispatcher.Client,Dispatcher.Op}",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.result",
-    "category": "Method",
-    "text": "result(client::Client, op::Dispatcher.Op)\n\nGather the result of the Op computation unit. Requires there to be at least one worker available to the scheduler or hangs indefinetely.\n\n\n\n"
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.cancel-Tuple{DaskDistributedDispatcher.Client,Array}",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.cancel",
-    "category": "Method",
-    "text": "cancel(client::Client, futures::Array)\n\nCancel all Ops in futures. This stops future tasks from being scheduled if they have not yet run and deletes them if they have already run. After calling, this result and all dependent results will no longer be accessible.\n\n\n\n"
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.gather-Tuple{DaskDistributedDispatcher.Client,Array}",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.gather",
-    "category": "Method",
-    "text": "gather(client::Client, futures::Array)\n\nGather the results of all Ops in futures. Requires there to be at least one worker available to the scheduler or hangs indefinetely.\n\n\n\n"
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.shutdown-Tuple{DaskDistributedDispatcher.Client}",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.shutdown",
-    "category": "Method",
-    "text": "shutdown(client::Client)\n\nTells the dask-scheduler to shutdown. This cancels all currently running tasks, clears the state of the scheduler, and shuts down all workers and scheduler. You only need to call this if you want to take down the distributed cluster.\n\n\n\n"
-},
-
-{
-    "location": "pages/api.html#DaskDistributedDispatcher.default_client-Tuple{}",
-    "page": "API",
-    "title": "DaskDistributedDispatcher.default_client",
-    "category": "Method",
-    "text": "default_client()\n\nReturn the default global client if a client has been registered with the dask-scheduler.\n\n\n\n"
 },
 
 {
@@ -185,11 +129,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "pages/api.html#DaskDistributedDispatcher.address-Tuple{DaskDistributedDispatcher.Worker}",
+    "page": "API",
+    "title": "DaskDistributedDispatcher.address",
+    "category": "Method",
+    "text": "address(worker::Worker)\n\nReturns this Workers's address formatted as an URI.\n\n\n\n"
+},
+
+{
+    "location": "pages/api.html#Base.show-Tuple{IO,DaskDistributedDispatcher.Worker}",
+    "page": "API",
+    "title": "Base.show",
+    "category": "Method",
+    "text": "show(io::IO, worker::Worker)\n\nPrints a representation of the worker and it's state.\n\n\n\n"
+},
+
+{
     "location": "pages/api.html#Worker-1",
     "page": "API",
     "title": "Worker",
     "category": "section",
-    "text": "Worker\nWorker(::String)"
+    "text": "Worker\nWorker(::String)\naddress(::Worker)\nshow(::IO, ::Worker)"
 },
 
 ]}
