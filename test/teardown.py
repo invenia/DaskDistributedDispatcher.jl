c3b1af658d81b59ba9a149afaff8f6a5fedde8ac
Nicole Epp <nicole.epp@invenia.ca>
2017-08-24 13:07:22 -0500

    Kill scheduler process directly on builds

--- test/teardown.py
+++
@@ -1,9 +0,0 @@
-from distributed import Client
-client = Client("tcp://127.0.0.1:8786")
-
-client.loop.add_callback(client.scheduler.retire_workers, close_workers=True)
-client.loop.add_callback(client.scheduler.terminate)
-client.run_on_scheduler(lambda dask_scheduler: dask_scheduler.loop.stop())
-
-# Source:
-# https://stackoverflow.com/questions/44021931/stopping-dask-ssh-created-scheduler-from-the-client-interface
